739fb2f0d69b08f97c5a3fcf21051d41f5f1d79f
wsgsc <411400309@qq.com>
2018-09-18 01:13:20 +0800

    1.禁止在任务栏显示　2.使用锁屏1px Activity　提高进程优先级

---
+++ app/src/main/java/com/king/ahh/abcd/activity/OnePixelActivity.java
@@ -0,0 +1,71 @@
+package com.king.ahh.abcd.activity;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.king.ahh.abcd.util.LogUtils;
+
+/**
+ * Created by gsc on 18-9-18.
+ */
+
+public class OnePixelActivity extends Activity {
+    private static final String TAG = "OnePixel";
+    private ScreenOffReceiver mScreenOffReceiver;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        LogUtils.log(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setGravity(Gravity.LEFT | Gravity.TOP);
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.x = 0;
+        params.y = 0;
+        params.height = 1;
+        params.width = 1;
+        window.setAttributes(params);
+
+
+        if (mScreenOffReceiver == null) {
+            mScreenOffReceiver = new ScreenOffReceiver();
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_SCREEN_ON);
+            registerReceiver(mScreenOffReceiver, filter);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mScreenOffReceiver != null)
+            unregisterReceiver(mScreenOffReceiver);
+            mScreenOffReceiver = null;
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    class ScreenOffReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                LogUtils.log(TAG,"fininsh OnePixelActivity");
+                OnePixelActivity.this.finish();
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/king/ahh/abcd/service/MainService.java
+++ app/src/main/java/com/king/ahh/abcd/service/MainService.java
@@ -1,9 +1,11 @@
 package com.king.ahh.abcd.service;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Binder;
 import android.os.IBinder;
@@ -11,7 +13,9 @@ import android.os.IBinder;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.king.ahh.abcd.activity.OnePixelActivity;
 import com.king.ahh.abcd.manager.ConnectionManager;
+import com.king.ahh.abcd.util.LogUtils;
 
 public class MainService extends Service {
     private static Context contextOfApplication;
@@ -19,6 +23,10 @@ public class MainService extends Service {
 
     private final IBinder mBinder = new MyBinder();
     private MyConn myConn = new MyConn();
+
+    private ScreenReceiver mScreenReceiver;
+    private static  final String TAG = "MainService";
+
     public MainService() {
     }
 
@@ -32,6 +40,14 @@ public class MainService extends Service {
             }
         });
 
+        //提升优先级
+        if (mScreenReceiver == null) {
+            mScreenReceiver = new ScreenReceiver();
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            registerReceiver(mScreenReceiver, filter);
+        }
+
 
     }
 
@@ -44,11 +60,10 @@ public class MainService extends Service {
 
 
     @Override
-    public int onStartCommand(Intent paramIntent, int paramInt1, int paramInt2)
-    {
+    public int onStartCommand(Intent paramIntent, int paramInt1, int paramInt2) {
         contextOfApplication = this;
         bindService(new Intent(this, RemoteService.class), myConn, Context.BIND_IMPORTANT);
-        if(mExceutorService.isTerminated()) {
+        if (mExceutorService.isTerminated()) {
             mExceutorService.execute(new Runnable() {
                 @Override
                 public void run() {
@@ -63,19 +78,21 @@ public class MainService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if (mScreenReceiver != null)
+            unregisterReceiver(mScreenReceiver);
+            mScreenReceiver = null;
     }
 
-    private void startAndBindService(){
+    private void startAndBindService() {
         startService(new Intent(this, RemoteService.class));
-        bindService(new Intent(this,RemoteService.class), myConn, Context.BIND_IMPORTANT);
+        bindService(new Intent(this, RemoteService.class), myConn, Context.BIND_IMPORTANT);
     }
 
-    public static Context getContextOfApplication()
-    {
+    public static Context getContextOfApplication() {
         return contextOfApplication;
     }
 
-    public class MyConn implements ServiceConnection{
+    public class MyConn implements ServiceConnection {
 
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -90,6 +107,17 @@ public class MainService extends Service {
 
     public class MyBinder extends Binder {
 
+    }
+
+    public class ScreenReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                LogUtils.log(TAG,"start OnePixelActivity");
+                Intent it = new Intent(context, OnePixelActivity.class);
+                context.startActivity(it);
+            }
+        }
     }
 
 
